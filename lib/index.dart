a0f8414124e03b7d28cf0d99ca01646e42cdf546
Harsh Tonde <harshtonde@gmail.com>
2023-09-17 00:49:00 +0530

    initial commit

---
+++ lib/index.dart
@@ -0,0 +1,5 @@
+// Export pages
+export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/pages/web_view_redirect/web_view_redirect_widget.dart'
+    show WebViewRedirectWidget;
+export '/pages/target_page/target_page_widget.dart' show TargetPageWidget;
